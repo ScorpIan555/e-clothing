149bafa0799b04637b80bf69416e77dbe70403d8
ScorpIan555 <ian.daley23@gmail.com>
2019-11-30 00:08:12 -0500

    added firebase & auth

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import { HomePage, ShopPage, ContactPage } from "./pages";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 import "./App.css";
 
@@ -19,10 +19,30 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      // createUserProfileDocument(user);
+      // console.log("user:::", user);
+      // this.setState({ currentUser: user });
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log("user:::", user);
+        userRef.onSnapshot(snapShot => {
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data()
+              }
+            },
+            () => {
+              console.log("this.state:::", this.state);
+            }
+          );
+        });
+        console.log(this.state);
+      } else {
+        this.setState({ currentUser: userAuth }); // s/b null
+      }
     });
   }
 
