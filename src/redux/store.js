b5d77ea1f61c9692a510a63e8b6e68f5cce6ae70
ScorpIan555 <ian.daley23@gmail.com>
2019-12-05 15:45:28 -0500

    turned off logs in production

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,10 @@ import { persistStore } from "redux-persist";
 // import state from reducer composition
 import rootReducer from "./root-reducer";
 // include array of middleware
-const middlewares = [logger];
+const middlewares = [];
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 // create store function
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 // add redux persistance tool to save user session objects
