fcab579282c769447de8c99106b29cf57f293e42
ScorpIan555 <ian.daley23@gmail.com>
2019-12-14 14:23:33 -0500

    fixed bug in manifest file, added package

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import "./index.css";
 import App from "./App";
 
 import { store, persistor } from "./redux/store";
-import * as serviceWorker from "./serviceWorker";
+import * as ServiceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <Provider store={store}>
